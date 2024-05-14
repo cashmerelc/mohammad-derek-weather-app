8dc49f4f5e2b8b718b6bdffacd405c9d461907a0
Derek Myers <cashmerelc@gmail.com>
2024-05-14 17:09:53 +0200

    Extract form data and reset form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,12 +1,26 @@
-export function Form() {
+export function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target.elements;
+    const formData = {
+      name: form.name.value,
+      isForGoodWeather: form.goodweather.checked,
+    };
+    onAddActivity(formData);
+
+    form.name.value = "";
+    form.goodweather.checked = false;
+    form.name.focus();
+  }
+
   return (
     <>
       <h1>Add New Activity</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Name:</label>
         <input type="text" name="name" />
         <label>Good-weather activity:</label>
-        <input type="checkbox" name="good-weather" />
+        <input type="checkbox" name="goodweather" />
         <button type="submit">Submit</button>
       </form>
     </>
