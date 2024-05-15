c2f550543152ba8f122e35a6ba474bea3309eff5
Derek Myers <cashmerelc@gmail.com>
2024-05-15 12:24:32 +0200

    Add fetch API to get weather from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Form } from "./components/Form/index.jsx";
 import { List } from "./components/List/index.jsx";
+import { useEffect } from "react";
 import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 
@@ -7,7 +8,19 @@ function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
-  const isGoodWeather = true;
+  const [weather, setWeather] = useLocalStorageState("weather", {});
+  let isGoodWeather = true;
+
+  useEffect(() => {
+    async function getWeather() {
+      const response = await fetch(
+        "https://example-apis.vercel.app/api/weather"
+      );
+      let weatherData = await response.json();
+      isGoodWeather = weatherData.isGoodWeather;
+    }
+    getWeather();
+  });
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, { ...newActivity }]);
