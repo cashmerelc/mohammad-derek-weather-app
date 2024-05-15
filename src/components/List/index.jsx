92b67be93c9c61bb2e5a07b898f86d81ea868bf2
Derek Myers <cashmerelc@gmail.com>
2024-05-15 16:18:10 +0200

    Fix styling issues

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -14,28 +14,32 @@ export function List({ list, filter, onDeleteActivity }) {
         </>
       ) : (
         <>
-          <h1 className="weather__icon">{filter.condition}</h1>
-          <h2 className="weather__temp">{filter.temperature}&deg; C</h2>
-          <h2 className="weather__title">
-            The weather is terrible, don't go out, instead do:
-          </h2>
+          <div className="weather__header">
+            <h1 className="weather__icon">{filter.condition}</h1>
+            <h2 className="weather__temp">{filter.temperature}&deg; C</h2>
+            <h2 className="weather__title">
+              The weather is terrible, don't go out, instead do:
+            </h2>
+          </div>
         </>
       )}
-      <ul>
-        {list.map((item) => {
-          return (
-            <li key={item.id}>
-              <p className="list__activityName">{item.name}</p>
-              <button
-                className="list__delete"
-                onClick={() => onDeleteActivity(item.id)}
-              >
-                x
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="list__container">
+        <ul>
+          {list.map((item) => {
+            return (
+              <li key={item.id}>
+                <p className="list__activityName">{item.name}</p>
+                <button
+                  className="list__delete"
+                  onClick={() => onDeleteActivity(item.id)}
+                >
+                  x
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 }
