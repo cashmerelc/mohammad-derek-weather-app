79d85a1e91a90b46ed8b6b1a2ddea3ad29679fae
Derek Myers <cashmerelc@gmail.com>
2024-05-15 10:37:22 +0200

    Import local storage state and add state to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { Form } from "./components/Form/index.jsx";
+import useLocalStorageState from "use-local-storate-state";
 import "./App.css";
 
 function App() {
-  return <Form />;
+  const [activities, setActivities] = useLocalStorageState("activities");
+
+  function handleAddActivity() {}
+
+  return <Form onAddActivity={handleAddActivity} />; // need to add prop onAddActivity and send the function that handles adding an activity. Also need to add the List component to the return
 }
 
 export default App;
